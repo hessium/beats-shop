700e6b1f2822d98f0b79aee9d2e817b962fe4794
Marat Davedov <hessium56@gmail.com>
2021-06-21 19:51:44 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,43 @@
+const buttonLeft = document.querySelector('#sliderLeft');
+const buttonRight = document.querySelector('#sliderRight');
+const itemsList = document.querySelector('#slider');
+const items = document.querySelector(".assortment__item");
+
+
+const step = items[0].getBoundingClientRect().width;
+const minRight = 0;
+const maxRight = (items.length - 1) + step; 
+
+let currentRight = 0;
+
+itemsList.style.right = currentRight;
+
+buttonRight.addEventListener("click", e => {
+  e.preventDefault();
+
+  if(currentRight === maxRight) {
+    currentRight = minRight;
+    itemsList.style.right = `${currentRight}px`;
+    return;
+  }
+
+  if(currentRight < maxRight) {
+    currentRight += step;
+    itemsList.style.right = `${currentRight}px`;
+  } 
+})
+
+buttonLeft.addEventListener("click", e => {
+  e.preventDefault();
+
+  if(currentRight === minRight) {
+    currentRight = maxRight;
+    itemsList.style.right = `${currentRight}px`;
+    return;
+  } 
+
+  if(currentRight > minRight) {
+    currentRight -= step;
+    itemsList.style.right = `${currentRight}px`;
+  } 
+})
