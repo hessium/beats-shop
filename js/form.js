66b566aad2339dd7fd02e280047bba5dc812883d
Marat Davedov <hessium56@gmail.com>
2021-07-02 21:32:45 +0300

    коммит

--- js/form.js
+++ js/form.js
@@ -1,48 +1,72 @@
-const form = document.querySelector('#myForm');
-const submitButton = document.querySelector('#sendForm');
+const validateFields = (form, fieldsArray) => {
+  fieldsArray.forEach((field) => {
+    field.removeClass("input-error");
+    if (field.val().trim() === "") {
+    field.addClass("input-error");
+    }
+  });
 
-function validateForm(form) {
-  let valid = true;
+  const errorFields = form.find(".input-error");
 
-  if(!validate(form.elements.name)) {
-    valid = false;
-  }
-  if(!validate(form.elements.phone))  {
-    valid = false;
-  }
-  if(!validate(form.elements.comment))  {
-    valid = false;
-  }
-  return valid;
+  return errorFields.length === 0;
 }
 
-function validate(element) {
-  
-  if (!element.checkValidity()) {
-    element.nextElementSibling.classList.add('form__error--active');
-    element.nextElementSibling.textContent = element.validationMessage;
-    element.style.border = "3px solid red";
-    return false;
-  } else  {
-    element.nextElementSibling.textContent = "";
-    element.nextElementSibling.classList.remove('form__error--active');
-    element.style.border = "3px solid transparent";
-    return true;
-}
-}
-
-submitButton.addEventListener("click", function (e) {
+$('.form').submit(e => {
   e.preventDefault();
 
-  if(validateForm(form)) {
-  
-  $.fancybox.open({
-    src: "#modal",
-    type: "inline"
-  })
-    
+  const form = $(e.currentTarget);
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']");
+  const to = form.find("[name='to']");
+
+  const modal = $('#modal');
+  const content = modal.find(".modal__content");
+
+  modal.removeClass("error-modal");
+
+  const isValid = validateFields(form, [name, phone, comment, to]);
+
+
+  if (isValid) {
+  const request = $.ajax({
+      url: "https://webdev-api.loftschool.com/sendmail",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val(),
+      }
+
+
+    });
+
+    request.done(data => {
+        content.text(data.message)
+    });
+
+    request.fail(data => {
+      const message = data.responseJSON.message;
+      content.text(message);
+      modal.addClass("error-modal");
+    });
+
+    request.always(() => {
+      $.fancybox.open({
+        src: "#modal",
+        type: "inline"
+      });
+    })
   }
+
 });
+
+$(".app-submit-btn").click(e => {
+  e.preventDefault();
+
+  $.fancybox.close();
+})
 
 $(".app-submit-btn").click(e => {
   e.preventDefault();
