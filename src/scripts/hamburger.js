9a7de61849e1140e7342ef8c5e7031ff668f070d
hessium <82901961+hessium@users.noreply.github.com>
2021-07-10 22:28:39 +0300

    Add files via upload

---
+++ src/scripts/hamburger.js
@@ -0,0 +1,21 @@
+let burger  = document.querySelector('.hamburger');
+let overlay = document.querySelector('.overlay');
+let body = document.querySelector('body');
+
+let links = document.querySelectorAll('.menu__link');
+
+links.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+
+ 
+})
+
+function toggleMenu(){
+  burger.classList.toggle('hamburger--active');
+  overlay.classList.toggle('overlay--active');
+  body.classList.toggle('body--active-menu');
+  blockScroll();
+}
+
+
+burger.addEventListener('click' , toggleMenu);
