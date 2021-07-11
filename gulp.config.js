636c3b91d3f8463e826a0f8eb7a23f256629e27e
hessium <82901961+hessium@users.noreply.github.com>
2021-07-11 17:43:48 +0300

    Commit 1

--- gulp.config.js
+++ gulp.config.js
@@ -5,6 +5,7 @@ module.exports = {
   JS_LIBS: [
     "node_modules/jquery/dist/jquery.min.js",
     "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js",
-    "node_modules/mobile-detect/mobile-detect.min.js"
+    "node_modules/mobile-detect/mobile-detect.min.js",
+    "node_modules/jquery-touchswipe/jquery.touchSwipe.min.js"
   ]
 };
